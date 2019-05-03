bc0c93b55c4e1f6169e187272a81d64217895d9f
Daniele <danielecassia9@gmail.com>
2019-05-03 11:19:21 -0300

    Seção 5 e 6

---
+++ src/vue-cli/proj-simples/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    {{ titulo }}
+    <button @click="titulo += '#'"> Alterar </button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return{
+      titulo: 'Teste de DATA usando VUE JS'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  background-color: cornflowerblue;
+  color: #fff;
+}
+</style>
+
+
